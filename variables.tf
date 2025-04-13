aba24be50c70a368429d74c4e198af842a08da33
Douglas Garstang <dgarstang@192-168-1-113.tpgi.com.au>
2025-04-13 18:19:26 +1000

    ...

--- variables.tf
+++ variables.tf
@@ -1,35 +1,58 @@
-variable "region" {
-}
-
-variable "zone" {
-}
-
-variable "subnet_id" {
-}
-
-variable "network_name" {
-}
-
-variable "ssh_public_key" {
-}
-
-variable "project_id" {
-}
-
-variable "gke_cluster_name" {
-}
-
-variable "subnets" {
-}
-
-variable "create_network" {
-  default = true
-}
-
 variable "create_bastion" {
-  default = true
+  type        = bool
+  default     = true
+  description = "Whether to create a bastion host"
 }
 
 variable "create_gke" {
-  default = true
+  type        = bool
+  default     = true
+  description = "Whether to create the GKE cluster"
 }
+
+variable "create_network" {
+  type        = bool
+  default     = true
+  description = "Whether to create the network"
+}
+
+variable "gke_cluster_name" {
+  type        = string
+  description = "Name of the GKE cluster"
+}
+
+variable "network_name" {
+  type        = string
+  description = "Name of the VPC network"
+}
+
+variable "project_id" {
+  type        = string
+  description = "Google Cloud project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "Region where resources will be deployed"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key for instance access"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "ID of the subnet to use"
+}
+
+variable "subnets" {
+  type        = map(any)
+  description = "Map of subnets to create, keyed by name"
+}
+
+variable "zone" {
+  type        = string
+  description = "Zone where resources will be deployed"
+}
+
